d2acea896a3abcbaccca64c4e7a6f1643f7ca220
Neila <neilaspace@outlook.com>
2025-08-25 17:52:48 +0800

    🌐使用Qt本地化

---
+++ src/locales/futili_zh_CN.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>cli</name>
+    <message>
+        <location filename="src/main.cpp" line="27"/>
+        <source>action</source>
+        <translation>动作</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="28"/>
+        <source>&quot;create&quot; or &quot;load&quot;.</source>
+        <translation>&quot;create&quot;或&quot;load&quot;。</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="29"/>
+        <source>file</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="30"/>
+        <source>URL of file to create or load backup.</source>
+        <translation>要创建或者加载备份的文件URL。</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="35"/>
+        <source>Too few arguments.</source>
+        <translation>过少的参数。</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="44"/>
+        <source>The file &quot;%1&quot; does not exist.</source>
+        <translation>文件“%1”不存在。</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="99"/>
+        <source>The action &quot;%1&quot; does not exist.</source>
+        <translation>动作“%1”不存在。</translation>
+    </message>
+</context>
+<context>
+    <name>dialog</name>
+    <message>
+        <location filename="src/main.cpp" line="65"/>
+        <source>Create backup of %1</source>
+        <translation>创建%1的备份</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="67"/>
+        <source>Backup name:</source>
+        <translation>备份名：</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="83"/>
+        <source>Load backup of %1</source>
+        <translation>加载%1的备份</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="84"/>
+        <source>Select the backup to load</source>
+        <translation>选择要加载的备份</translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,41 @@
 #include <QDir>
 #include <QFile>
 #include <QInputDialog>
-#include <KLocalizedString>
+#include <QTranslator>
 
 #define BACKUPSDIRNAME "." + QStringLiteral(NAME) + "_backup"
 
-int main(
-    int argc, char *argv[]) {
-    qDebug() << 3;
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QApplication::setApplicationName(NAME);
     QApplication::setApplicationVersion(VERSION);
-    KLocalizedString::setApplicationDomain(QByteArrayLiteral(
-        NAME)); // https://api.kde.org/frameworks/ki18n/html/prg_guide.html#link_prog
-    qDebug() << KLocalizedString::availableDomainTranslations(NAME)
-             << KLocalizedString::availableApplicationTranslations()
-             << KLocalizedString::languages()
-             << KLocalizedString::isApplicationTranslatedInto("zh_CN") << i18n("action");
+
+    QTranslator translator;
+    if (translator.load(QLocale(), NAME, "_", ":/i18n"))
+        a.installTranslator(&translator);
 
     QCommandLineParser parser;
     parser.addHelpOption();
     parser.addVersionOption();
-    parser.setApplicationDescription(i18n(DESCRIPTION));
-
-    parser.addPositionalArgument(i18n("action"), i18n("\"create\" or \"load\"."));
-    parser.addPositionalArgument(i18n("file"), i18n("URL of file to create or load backup."));
+    parser.setApplicationDescription(a.translate("main", DESCRIPTION));
+    parser.addPositionalArgument(a.translate("cli", "action"),
+                                 a.translate("cli", "\"create\" or \"load\"."));
+    parser.addPositionalArgument(a.translate("cli", "file"),
+                                 a.translate("cli", "URL of file to create or load backup."));
     parser.process(a);
 
     const QStringList args = parser.positionalArguments();
     if (args.size() < 2) {
-        qCritical().noquote() << i18n("Too few arguments.");
+        qCritical().noquote() << a.translate("cli", "Too few arguments.");
         return EXIT_FAILURE;
     }
     const QString action = args.first();
     const QUrl filePathUrl(args.at(1));
     const QString filePath = filePathUrl.toLocalFile();
     QFile file(filePath);
-    qDebug() << filePathUrl;
     if (!file.exists()) {
-        qCritical().noquote() << i18n("The file \"%1\" does not exist.", filePath);
+        qCritical().noquote() << a.translate("cli", "The file \"%1\" does not exist.")
+                                     .arg(filePath);
         return EXIT_FAILURE;
     }
 
@@ -60,41 +57,39 @@ int main(
 
     bool ok{};
     if (action == "create") {
-        const QString backupName = QInputDialog::getText(nullptr,
-                                                         i18n("Create backup of %1", filePath),
-                                                         i18n("Backup name:"),
-                                                         QLineEdit::Normal,
-                                                         QDateTime::currentDateTime().toString(
-                                                             "yyyyMMdd-HHmmss"),
-                                                         &ok,
-                                                         Qt::Window,
-                                                         Qt::ImhNoAutoUppercase);
+        const QString backupName = QInputDialog::
+            getText(nullptr,
+                    a.translate("dialog", "Create backup of %1").arg(filePath),
+                    a.translate("dialog", "Backup name:"),
+                    QLineEdit::Normal,
+                    QDateTime::currentDateTime().toString("yyyyMMdd-HHmmss"),
+                    &ok,
+                    Qt::Window,
+                    Qt::ImhNoAutoUppercase);
         if (ok && !backupName.isEmpty()) {
             thisBackupDir.remove(backupName);
             file.copy(thisBackupDir.path() + QDir::separator() + backupName);
-            qDebug() << filePathList << fileDir.path() << backupsDir.path() << thisBackupDir.path()
-                     << thisBackupDir.path() + QDir::separator() + backupName;
         }
     } else if (action == "load") {
-        const QString backupToLoad
-            = QInputDialog::getItem(nullptr,
-                                    i18n("Load backup of %1", filePath),
-                                    i18n("Select the backup to load"),
-                                    thisBackupDir.entryList(QDir::Files | QDir::Readable
-                                                                | QDir::NoDotDot,
-                                                            QDir::Time),
-                                    0,
-                                    true,
-                                    &ok,
-                                    Qt::Window,
-                                    Qt::ImhNoAutoUppercase);
+        const QString backupToLoad = QInputDialog::
+            getItem(nullptr,
+                    a.translate("dialog", "Load backup of %1").arg(filePath),
+                    a.translate("dialog", "Select the backup to load"),
+                    thisBackupDir
+                        .entryList(QDir::Files | QDir::Readable | QDir::NoDotDot, QDir::Time),
+                    0,
+                    true,
+                    &ok,
+                    Qt::Window,
+                    Qt::ImhNoAutoUppercase);
         if (ok) {
             file.remove();
             QFile selectedBackup(thisBackupDir.path() + QDir::separator() + backupToLoad);
             selectedBackup.copy(filePath);
         }
     } else {
-        qCritical().noquote() << i18n("The action \"%1\" does not exist.", action);
+        qCritical().noquote() << a.translate("cli", "The action \"%1\" does not exist.")
+                                     .arg(filePath);
         return EXIT_FAILURE;
     }
 
